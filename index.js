9bf42f444d3a1a2355c4e19c5bfb484912dfaf3c
IMATIC\tarasov.a <tarasov.a@imatic.pro>
2018-09-08 16:24:58 +0300

    app with vk api

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-/*
-Rx.Observable.of([1, 2, 3, 4, 5, 'vfv'], 12, 'string')
-    .subscribe(
-        (x) => console.log('of: ' + x),
-        (error) => console.log('Error: ' + err),
-        () => console.log('Completed')
-    );
-*/
+function getUserById(id) {
+    const params = {
+        access_token: '0039a8a0debac447d8c2c165fa7fab325e7826e19b2a0f8806d63bcd2ab32e3d49db6568ca1b51a30730b',
+        user_ids: id,
+        fields: 'photo_big'
+    };
 
-Rx.Observable.of('aa', 'bb', 'vv')
-    .map(x => {
-        return {
-            value: x,
-            f: x.length
-        };
-    })
+    return $.ajax({
+        url: 'https://api.vk.com/method/users.get?' + $.param(params) + '&v=3.0',
+        type: 'GET',
+        dataType: 'JSONP'
+    }).promise();
+}
+
+Rx.Observable.fromEvent($('input'), 'keyup')
+    .pluck('target', 'value')
+    .distinct()
+    .debounce(2000)
+    .mergeMap(v => Rx.Observable.fromPromise(getUserById(v)))
+    .catch(error => Rx.Observable.of(error))
+    .map(x => x.response[0])
     .subscribe(
-        (x) => console.log(x.f)
+        (user) => {
+            $('h1').html(`${user.first_name} ${user.last_name} <i>${user.uid}</i>`);
+            $('img').attr('src', user.photo_big);
+        },
+        error => console.log(error),
+        () => console.log('Completed')
     );
