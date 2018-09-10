ea86c83b7a1c9deda1a96a0f673e2edac64397b6
IMATIC\tarasov.a <tarasov.a@imatic.pro>
2018-09-10 14:36:07 +0300

    info on vk id

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function getUserById(id) {
     const params = {
-        access_token: '0039a8a0debac447d8c2c165fa7fab325e7826e19b2a0f8806d63bcd2ab32e3d49db6568ca1b51a30730b',
+        access_token: 'acab1c4d26c4103715a983b69feacb919a8cf51a2ddb4c69b709bbd217c1004f81900130d24299c499754',
         user_ids: id,
-        fields: 'photo_big,online'
+        fields: 'photo_100,online'
     };
 
     return $.ajax({
@@ -21,9 +21,18 @@ Rx.Observable.fromEvent($('input'), 'keyup')
     .map(x => x.response[0])
     .subscribe(
         (user) => {
+            /*var online = user.online ? 'online' : 'offline';
+            $('ul').html(`
+                <li>${user.first_name} ${user.last_name}</li> 
+                <li>id = ${user.id}</li> 
+                <li>status = ${online}</li>
+            `);*/
             var online = user.online ? 'online' : 'offline';
-            $('h1').html(`${user.first_name} ${user.last_name} <i>${user.uid} ${user.online}</i>`);
-            $('img').attr('src', user.photo_big);
+            $('a').attr('href','http://vk.com/id' + user.id);
+            $('h4').html(`${user.first_name} ${user.last_name}`);
+            $('p').html(`${online}`);
+            $('img').attr('src', user.photo_100);
+
         },
         error => console.log(error),
         () => console.log('Completed')
